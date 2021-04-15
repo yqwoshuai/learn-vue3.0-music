8e40604e1358f18400c15e00e625c4cf9819ae0e
yqwoshuai <358805791@qq.com>
2021-04-15 20:32:10 +0800

    切换播放模式

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -14,7 +14,7 @@
       <div class="bottom">
         <div class="operators">
           <div class="icon i-left">
-            <i class="icon-sequence"></i>
+            <i :class="modeIcon" @click="changeMode"></i>
           </div>
           <div class="icon i-left" :class="disableCls">
             <i @click="prev" class="icon-prev"></i>
@@ -43,6 +43,7 @@
 <script>
 import { computed, watch, ref } from 'vue'
 import { useStore } from 'vuex'
+import useMode from './use-mode'
 
 export default {
   name: 'player',
@@ -176,6 +177,9 @@ export default {
       songReady.value = true
     }
 
+    // 封装切换播放模式逻辑
+    const { modeIcon, changeMode } = useMode()
+
     return {
       error,
       disableCls,
@@ -188,7 +192,10 @@ export default {
       goBack,
       audioRef,
       fullScreen,
-      currentSong
+      currentSong,
+      // useMode
+      modeIcon,
+      changeMode
     }
   }
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -3,7 +3,7 @@ import shuffle from '@/assets/js/util'
 
 // 执行顺序播放
 export function selectPlay({ commit }, { list, index }) {
-  commit('setPlayMode', PLAY_MODE.sqeuence)
+  commit('setPlayMode', PLAY_MODE.sequence)
   commit('setSequenceList', list)
   commit('setPlayingState', true)
   commit('setFullScreen', true)
@@ -19,4 +19,23 @@ export function randomPlay({ commit }, list) {
   commit('setFullScreen', true)
   commit('setPlayList', shuffle(list))
   commit('setCurrentIndex', 0)
+}
+
+// 执行切换播放模式
+export function changeMode({ commit, state, getters }, mode) {
+  // 切换前保存当前播放歌曲的id，因为切换会修改当前播放list
+  // 当前播放索引没有变化导致对应的歌曲变化
+  const currentId = getters.currentSong.id
+  // 随机模式洗牌播放列表
+  if (mode === PLAY_MODE.random) {
+    commit('setPlayList', shuffle(state.sequenceList))
+  } else {
+    commit('setPlayList', state.sequenceList)
+  }
+  // 找到之前歌曲对应的新index
+  const index = state.playList.findIndex(song => {
+    return song.id === currentId
+  })
+  commit('setCurrentIndex', index)
+  commit('setPlayMode', mode)
 }
